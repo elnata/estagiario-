8cbfabbb66c046fe82adf2a7adbddb182c6c2faf
elnata <elnatan.des@gmail.com>
2016-10-19 13:18:50 -0300

    sistema

--- area/info/mail/config.php
+++ area/info/mail/config.php
@@ -1,10 +1,10 @@
 <?
 error_reporting (E_ALL & ~ E_NOTICE & ~ E_DEPRECATED);
-define('BD_USER', 'u963376040_root'); // USE O TEU USUÁRIO DE BANCO DE DADOS
-define('BD_PASS', '123456'); // USE A TUA SENHA DO BANCO DE DADOS
-define('BD_NAME', 'u963376040_escol'); // USE O NOME DO TEU BANCO DE DADOS
+define('BD_USER', 'natan'); // USE O TEU USUÁRIO DE BANCO DE DADOS
+define('BD_PASS', 'Na@2483365'); // USE A TUA SENHA DO BANCO DE DADOS
+define('BD_NAME', 'escola'); // USE O NOME DO TEU BANCO DE DADOS
 
-mysql_connect('mysql.hostinger.com.br', BD_USER, BD_PASS);
+mysql_connect('MYSQLCONNSTR_localdb', BD_USER, BD_PASS);
 mysql_select_db(BD_NAME);
 
 
